b24326abf1e485590a666b16d2d253aeeb7ede28
nds-najam <najam.iitm@gmail.com>
2023-04-13 11:52:59 +0530

    logging n docstrings added (auto docstring )

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from utils.all_utils import prepare_data, save_plot
 from utils.model import Perceptron
 import pandas as pd
+import logging
+import os
+
+log_dir = "logs"
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(
+    filename=os.path.join(log_dir,"running_logs.log"),
+    level=logging.INFO,
+    format='[%(asctime)s:%(levelname)s:%(module)s]: %(message)s',
+    filemode='a'
+     )
 
 def main(data,modelName,plotName,eta,epochs):
     df = pd.DataFrame(data)
+    logging.info(f"This is the raw dataset: \n{df}")
     X, y = prepare_data(df)
     model = Perceptron(eta=eta, epochs=epochs)
     model.fit(X,y)
@@ -16,24 +28,31 @@ def main(data,modelName,plotName,eta,epochs):
 if __name__ == "__main__":
     ETA = 0.1
     EPOCHS = 10
-
-    OR = {
-        "x1": [0,0,1,1],
-        "x2": [0,1,0,1],
-        "y" : [0,1,1,1]
-    }
-    main(data=OR, modelName="or.model",plotName="or.png", eta=ETA, epochs=EPOCHS)
-
-    AND = {
-        "x1": [0,0,1,1],
-        "x2": [0,1,0,1],
-        "y" : [0,0,0,1]
-    }
-    main(data=AND, modelName="and.model",plotName="and.png", eta=ETA, epochs=EPOCHS)
-
-    XOR = {
-        "x1": [0,0,1,1],
-        "x2": [0,1,0,1],
-        "y" : [0,1,1,0]
-    }
-    main(data=XOR, modelName="xor.model",plotName="xor.png", eta=ETA, epochs=EPOCHS)
+    gate = 'OR'
+    if gate == 'OR':
+        DATA = {
+            "x1": [0,0,1,1],
+            "x2": [0,1,0,1],
+            "y" : [0,1,1,1]
+        }
+    elif gate == 'AND':
+        DATA = {
+            "x1": [0,0,1,1],
+            "x2": [0,1,0,1],
+            "y" : [0,0,0,1]
+        } 
+    elif gate == 'XOR':
+        DATA = {
+            "x1": [0,0,1,1],
+            "x2": [0,1,0,1],
+            "y" : [0,1,1,0]
+        }
+    else:
+        logging.info("Bad GATE chosen")
+    try:
+        logging.info(f">>>>> start training for {gate} gate <<<<<")
+        main(data=DATA, modelName=gate+".model",plotName=gate+".png", eta=ETA, epochs=EPOCHS)
+        logging.info(f">>>>> done training for {gate} gate <<<<<\n\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -3,8 +3,19 @@ import os
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
+import logging
 
 def prepare_data(df, target_col='y'):
+    """_summary_
+
+    Args:
+        df (pd.DataFrame): This is a dataframe
+        target_col (str, optional): Label column name. Defaults to 'y'.
+
+    Returns:
+        tuple: label and X
+    """
+    logging.info("Preparing data for training")
     X = df.drop(target_col, axis=1)
     y = df[target_col]
 
@@ -12,6 +23,12 @@ def prepare_data(df, target_col='y'):
 
 def save_plot(df, model, filename="plot.png", plot_dir = "plots"):
     def _create_base_plot(df):
+        """_summary_
+
+        Args:
+            df (pd.DataFrame): Pandas dataframe
+        """
+        logging.info("Creating the base plot")
         df.plot(kind='scatter',x='x1',y='x2',c='y', s=100, cmap="coolwarm")
         plt.axhline(y=0, color='black', linestyle='--',linewidth=1)
         plt.axvline(x=0, color='black', linestyle='--',linewidth=1)
@@ -20,6 +37,7 @@ def save_plot(df, model, filename="plot.png", plot_dir = "plots"):
         figure.set_size_inches(10,8)
 
     def _plot_decision_regions(X, y, classifier, resolution=0.02):
+        logging.info("Creating decision regions")
         colors = ('cyan','lightgreen')
         cmap = ListedColormap(colors)
 
@@ -53,6 +71,4 @@ def save_plot(df, model, filename="plot.png", plot_dir = "plots"):
     os.makedirs(plot_dir, exist_ok=True)
     plot_path = os.path.join(plot_dir, filename)
     plt.savefig(plot_path)
-
-
-
+    logging.info(f"Saving the plot at {plot_path}")
